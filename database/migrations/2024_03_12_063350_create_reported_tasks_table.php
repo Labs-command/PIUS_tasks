114012b7bbf3b346fea2b9c372ee8965d0da95ae
DaniilPankov <giftedkek@gmail.com>
2024-03-12 10:16:41 +0300

    Migration add table tasks added

--- database/migrations/2024_03_12_063350_create_reported_tasks_table.php
+++ database/migrations/2024_03_12_063350_create_reported_tasks_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('reported_tasks', function (Blueprint $table) {
+            $table->uuid("task_id");
+            $table->string("subject", 32);
+            $table->text("text");
+            $table->text("answer");
+            $table->text("reason_comment");
+            $table->uuid("author_id");
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reported_tasks');
     }
 };
