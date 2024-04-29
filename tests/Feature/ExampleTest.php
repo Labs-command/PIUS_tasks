7f7e02ebb4075bfcf05112b6ed196d4cb287379e
DaniilPankov <giftedkek@gmail.com>
2024-04-29 13:09:06 +0300

    Test-example are working

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/tasks/');
+        $response = $this->post('/tasks/api/v1/reported-tasks/search');
 
         $response->assertStatus(200);
     }
